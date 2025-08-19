ceb0fa6fa1eb63b8b4dae5734dd40812774e3b35
Nannoy <nannoyprogramming@gmail.com>
2025-08-19 23:41:55 +0100

    comment

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,3 +1,12 @@
+//This is a comment, It can not be execute
+
+//This is a also a comment,
+//but over multiple lines
+
+/* This is a comment over multiple lines
+like many
+many lines. */
+
 void main() {
   print("Hello World!");
 }
