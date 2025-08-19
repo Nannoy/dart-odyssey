44dfb74b758630896582b215a94f191b7ed3f08b
Nannoy <nannoyprogramming@gmail.com>
2025-08-19 23:48:30 +0100

    statements and expressions

---
+++ bin/2_statements_and_expressions.dart
@@ -0,0 +1,7 @@
+void main() {
+/* expression = value (an expression is a value)
+like: 42, 3 + 2, 'Hello!,
+*/
+
+  print("Hello World!"); //this line is a statement, statements ends with semicolon (;)
+}
